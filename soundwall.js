b2e4294cfc8f0e816b9b49367e2c8d61b35d8800
teak1 <trainian@gmail.com>
2019-06-23 19:26:34 -0700

    Add files via upload

--- soundwall.js
+++ soundwall.js
@@ -1,7 +1,6 @@
 //version 0.0.2
 function soundwall(o, mount) {
-  var isMobile = false; //initiate as false
-  // device detection
+  var isMobile = false;
   if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
     isMobile = true;
@@ -24,16 +23,21 @@ function soundwall(o, mount) {
   let activated = false;
   mount.appendChild(html(`<div>
   <style>
-  .soundwall-container {position: relative;width: 300px;background-color:rgba(200,200,200,0.6);}
+  .soundwall-container {position: relative;width: 500px;background-color:rgba(200,200,200,0.6);}
   .soundwall-image {opacity: 1;display: block;width: 100%;height: auto;transition: .2s ease;backface-visibility: hidden;}
   .soundwall-middle {transition: .2s ease;opacity: 0;position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);-ms-transform: translate(-50%, -50%);text-align: center;}
   .soundwall-container:hover .soundwall-image {opacity: 0.3;}
   .soundwall-container:hover .soundwall-middle {opacity: 1;}
+  .soundwall-container:hover .soundwall-text {opacity: 1;}
   .soundwall-open-mob{position: absolute;
     width: 100%;
     padding-top: 1em;
     padding-bottom: 0.1em;
     background-color:rgba(200,200,200,0.9);text-align:center;height:2em;}
+  .soundwall-text{
+    font-size:2em;
+    width:500px;
+  }
   </style>
   <div class="soundwall-container" id="${openId}">
   
@@ -42,7 +46,7 @@ function soundwall(o, mount) {
   ${isMobile ? "" : `
   <div class="soundwall-middle">
     <a>
-    <div class="soundwall-text">Click to open soundwall</div>
+    <div class="soundwall-text"><b>Click to open soundwall</b></div>
     </a>
   </div>
   `}
@@ -58,18 +62,14 @@ function soundwall(o, mount) {
         left:0;
         width:100vw;
         height:100vh;
-        transition:0.5s;
         background-color:rgba(200,200,200,0.6);
         opacity:0;
         pointer-events:none;
-        transition-propery:opacity;
         z-index:100;
       }
       .soundwall-display-wrapper.open{
-        transition:0.5s;
         opacity:1;
         pointer-events:all;
-        transition-propery:opacity;
       }
       .soundwall-display-close-button{
         position:fixed;
@@ -102,7 +102,7 @@ function soundwall(o, mount) {
     <div class="soundwall-overlay-container">
     <div class="soundwall-overlay-middle">
     <div id="${wrapperId}">
-    <img class="soundwall-overlay-image-horizontal" id="${imgId}"/>
+    <img class="${isMobile ? "soundwall-overlay-image-horizontal" : "soundwall-overlay-image-vertical"}" id="${imgId}"/>
     </div>
   </div>
   </div>
@@ -114,7 +114,8 @@ function soundwall(o, mount) {
 
   const maskImg = document.createElement("img");
   const wrapper = document.getElementById(wrapperId);
-  function handleCloseOverlay() {
+  function handleCloseOverlay(event) {
+    event.preventDefault();
     overlay.classList.remove("open");
     overlay.hidden = true;
     const keys = Object.keys(lookup);
@@ -124,7 +125,8 @@ function soundwall(o, mount) {
   }
   document.getElementById(buttonId).addEventListener("click", handleCloseOverlay);
   document.getElementById(buttonId).addEventListener("touchstart", handleCloseOverlay);
-  function handleOpen() {
+  function handleOpen(event) {
+    event.preventDefault();
     overlay.hidden = false;
     overlay.classList.add("open");
     if (!activated) {
@@ -134,8 +136,8 @@ function soundwall(o, mount) {
       }
       activated = true;
       activate();
-      window.requestAnimationFrame(() => { updateImageSize() });
     }
+    updateImageSize()
   }
   document.getElementById(openId).addEventListener("click", handleOpen);
   document.getElementById(openId).addEventListener("touchstart", handleOpen);
@@ -144,8 +146,9 @@ function soundwall(o, mount) {
     loaded++;
     if (loaded == 2) execute();
   }
-  function activate() {
+  function activate(event) {
 
+    event.preventDefault();
     maskImg.addEventListener("load", loadHandler);
     maskImg.src = images.mask;
   }
@@ -166,7 +169,6 @@ function soundwall(o, mount) {
       let x = Math.floor(evt.offsetX / img.width * maskImg.width);
       let y = Math.floor(evt.offsetY / img.height * maskImg.height);
       let pixel = format(ctx.getImageData(x, y, 1, 1).data);
-      console.log(x, y);
       if (lookup[pixel]) lookup[pixel].play();
     })
   }
@@ -175,19 +177,6 @@ function soundwall(o, mount) {
     let boundingrect = img.getClientRects()[0];
     if (!boundingrect) return;
     let current = img.getAttribute("class");
-    // if (window.innerWidth < window.innerHeight) {
-    //   if (img.width > img.height) {
-    //     orientation = "horizontal"
-    //   } else {
-    //     orientation = "vertical"
-    //   }
-    // } else {
-    //   if (img.width > img.height && img.getClientRects()[0] && img.getClientRects()[0].height <= window.innerHeight - 100) {
-    //     orientation = "horizontal"
-    //   } else {
-    //     orientation = "vertical"
-    //   }
-    // }
     if (current === "soundwall-overlay-image-horizontal") {
       if (boundingrect.height > window.innerHeight - 100) {
         orientation = "vertical";
